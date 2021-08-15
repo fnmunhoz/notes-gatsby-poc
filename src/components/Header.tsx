5a13357f184fce5ccec55ffffb69ebc02a7ecca4
Felipe Munhoz <felipe@felipemunhoz.com>
2021-08-15 11:35:48 -0300

    feat: add basic theme support
    References:
    
    https://www.joshwcomeau.com/blog/how-i-built-my-blog
    
    https://epicreact.dev/css-variables
    
    https://philipwalton.github.io/solved-by-flexbox/demos/sticky-footer
    
    Icons:
    
    https://fonts.google.com/icons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,32 @@
 import * as React from "react"
+import styled from "@emotion/styled"
 import { Link } from "gatsby"
+import theme from "../styles/theme"
+import ThemeToggler from "../styles/ThemeToggler"
 
 const Header = ({ siteTitle = `` }: { siteTitle: string }) => (
-  <header>
-    <div>
-      <h1>
-        <Link to="/">
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
+  <Container>
+    <h1>
+      <Link to="/">{siteTitle}</Link>
+    </h1>
+    <ThemeToggler />
+  </Container>
 )
+
+const Container = styled.header`
+  display: flex;
+  padding: 20px;
+
+  a {
+    text-decoration: none;
+    font-weight: bolder;
+    color: ${theme.colors.primary};
+  }
+
+  h1 {
+    flex: 1;
+    margin: 0;
+  }
+`
 
 export default Header
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,12 +5,18 @@
  * See: https://www.gatsbyjs.com/docs/use-static-query/
  */
 
+import "normalize.css"
+import "../styles/vars.css"
+import theme from "../styles/theme"
 import * as React from "react"
+import styled from "@emotion/styled"
+import { Global, css } from "@emotion/react"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./Header"
+import Footer from "./Footer"
 
-const Layout = ({ children }: { children: React.ReactNode}) => {
+const Layout = ({ children }: { children: React.ReactNode }) => {
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
@@ -23,12 +29,34 @@ const Layout = ({ children }: { children: React.ReactNode}) => {
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata?.title} />
-      <div>
-        <main>{children}</main>
-      </div>
+      <Global
+        styles={css`
+          body {
+            color: ${theme.colors.primary};
+            background-color: ${theme.colors.background};
+          }
+        `}
+      />
+      <Container>
+        <Header siteTitle={data.site.siteMetadata?.title} />
+        <Content>{children}</Content>
+        <Footer />
+      </Container>
     </>
   )
 }
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  margin-left: auto;
+  margin-right: auto;
+  max-width: 42rem;
+`
+
+const Content = styled.main`
+  flex: 1;
+`
 
 export default Layout
