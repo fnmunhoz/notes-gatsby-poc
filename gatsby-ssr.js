3be5dfa111a62f826feb7a38c047dc36a8cb9228
Felipe Munhoz <felipe@felipemunhoz.com>
2021-08-15 13:59:19 -0300

    feat: set dark theme via ssr so the page doesn't blink

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,4 +4,8 @@
  * See: https://www.gatsbyjs.com/docs/ssr-apis/
  */
 
-// You can delete this file if you're not using it
+exports.onRenderBody = ({ setBodyAttributes }) => {
+  setBodyAttributes({
+    "data-theme": "dark",
+  })
+}
